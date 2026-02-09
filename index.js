f7e40f51e3d30a99e4d38b3a7342148ef5a66b87
ansid0109 <sidh0109@algonquinlive.com>
2026-02-09 15:31:42 -0500

    serving product

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/products/:id', cors(), function (req, res, next) {
-  res.json({msg: 'Hello'})
+const products = [
+    { id: 1, name: 'Dog Food', price: 19.99 },
+    { id: 2, name: 'Cat Food', price: 34.99 },
+    { id: 3, name: 'Bird Seeds', price: 10.99 }
+];
+
+app.get('/products', cors(), function (req, res, next) {
+    res.json(products);
 });
 
 const port = 3030;
